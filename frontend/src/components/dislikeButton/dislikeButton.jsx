546be93153be71e4b0c7d11d9650c27abc9aa154
fred <fredwright95@yahoo.com>
2022-05-04 11:25:31 -0400

    started dislikebutton functionality

---
+++ frontend/src/components/dislikeButton/dislikeButton.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+
+
+const disLikeButton = (props) => {
+    const [user, token] = useAuth();
+
+    const dislikereview = async () => {
+        try {
+          await axios.put(
+            `http://127.0.0.1:8000/api/comments/dislike/${props.review_id}/`,
+            {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+        } catch (error) {
+          console.log(error.message);
+        }
+    
+    }
+    
+    
+    function handledisLike() {
+        dislikereview();
+      }
+    
+    
+    
+    return ( 
+    <div key={props.review_id}>
+      <button className="dislike-button" onClick={handledisLike}></button>
+    </div>
+     );
+}
+ 
+export default disLikeButton;
